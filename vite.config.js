dcaea09eb85ea6192eb66acb2efcfa9467521d42
SacPolisson9806 <leparouxenzo2@gmail.com>
2025-11-05 14:16:46 +0100

    aa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { copyFileSync } from 'fs';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+  },
+  // Hook pour copier _redirects après le build
+  closeBundle() {
+    copyFileSync('_redirects', 'dist/_redirects');
+  }
+});
